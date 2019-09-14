79323fc5403dc935267ad3fe6626fb378cd42ad7
Rolf Nelson <54694694+SuleymanTheMagnificant@users.noreply.github.com>
2019-09-14 11:12:46 -0400

    Update Saccadic_POV_32_LEDs.ino

--- Teensy code (Arduino IDE)/Saccadic_POV_32_LEDs.ino
+++ Teensy code (Arduino IDE)/Saccadic_POV_32_LEDs.ino
@@ -1,7 +1,6 @@
 /*
 *Persistence of Vision for use in saccade-based presentations. Uses APA102 (Dotstar) leds, sending colour values from
-*an array held in flash memory (designated by 'const'). 
-API for M5Stack is here https://docs.m5stack.com/#/en/api/lcd
+*an array held in flash memory (designated by 'const'). Presented on a Teensy 4.0.
 Documentation for FastLed is here: https://github.com/FastLED/FastLED/wiki
 */
 
@@ -9,8 +8,8 @@ Documentation for FastLed is here: https://github.com/FastLED/FastLED/wiki
 #include "FastLED.h"
 
 #define NUM_LEDS 32 //number of leds in strip 
-#define DATA_PIN 21// spi data 
-#define CLOCK_PIN 22//spi clock
+#define DATA_PIN 11// spi data 
+#define CLOCK_PIN 13//spi clock
 #define COLOR_ORDER BGR //otherwise B&R get swapped
 
 int numberOfSlices = 32;
@@ -19,7 +18,7 @@ CRGB leds[NUM_LEDS];
 
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);// http://fastled.io/docs/3.1/group___color_enums.html
   delay(200);
   FastLED.setTemperature(TypicalSMD5050); // http://fastled.io/docs/3.1/group___color_enums.html
