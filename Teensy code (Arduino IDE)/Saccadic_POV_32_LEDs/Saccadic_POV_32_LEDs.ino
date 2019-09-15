a0e0ac26230a89be7f26412f875a6b3049ca07c4
Jason Goodman <jcgoodman@gmail.com>
2019-09-15 18:54:26 -0400

    Serial input
    Added serial input for POV displays.  Command language:
    'bN': set brightness to N, where N is '0' through '9'.
    'c': cycle through all available images (initially active on startup)
    'pN' pick image N to show next
    'o': turn display off
    's': show current image for 4 sec.

--- Teensy code (Arduino IDE)/Saccadic_POV_32_LEDs/Saccadic_POV_32_LEDs.ino
+++ Teensy code (Arduino IDE)/Saccadic_POV_32_LEDs/Saccadic_POV_32_LEDs.ino
@@ -13,8 +13,22 @@ Documentation for FastLed is here: https://github.com/FastLED/FastLED/wiki
 #define CLOCK_PIN 13//spi clock
 #define COLOR_ORDER BGR //otherwise B&R get swapped
 
+// Serial command verbs
+#define POV_NO_MSG 0
+#define POV_SHOW 's'
+#define POV_OFF 'o'
+#define POV_BRIGHTNESS 'b'
+#define POV_PICK 'p'
+#define POV_CYCLE 'c'
+
+bool cycle = true;
 int numberOfSlices = 32;
 uint8_t CurrentBrightness = 255; //scaling function
+int display_duration = 4000;
+unsigned long time_to_stop = millis()+display_duration;
+
+unsigned int CurrentImage = 0;
+const unsigned int *imagelib[3] = {mario,zero,cross};
 
 /* Put variables to be controlled by PsychoPy via Serial.read() here
 CurrentImage  --> what image is being presented
@@ -26,6 +40,66 @@ uint8_t CurrentBrightness = 255 --> Scaling function
 */
 CRGB leds[NUM_LEDS];
 
+ 
+// Persistent serial variables
+byte lastmsg; // Last command verb received
+int expect; // How many more bytes to expect for lastmsg
+bool showing; // Is image currently being displayed?
+
+void serial_handler() {
+  // put your main code here, to run repeatedly:
+  byte msg = 0;   // Serial message verb id
+  if (msg = Serial.read()) {
+    if (expect) {
+      expect--;
+      switch (lastmsg) {
+        case POV_BRIGHTNESS:
+          CurrentBrightness = (msg-'0')*255/9;
+          Serial.println(CurrentBrightness);
+          FastLED.setBrightness(CurrentBrightness);
+          lastmsg = POV_NO_MSG;
+          break;
+        case POV_PICK:
+          CurrentImage = msg-'0';
+          Serial.println(CurrentImage);
+          lastmsg = POV_NO_MSG;
+          break;
+        default:
+          break;
+      }
+    }
+    switch (msg) {
+      case POV_SHOW:
+        Serial.println("Displaying picture.");
+        showing = true;
+        cycle = false;
+        time_to_stop = millis() + 4000;
+        break;
+      case POV_BRIGHTNESS:
+        Serial.print("Setting brightness to ");
+        expect = 1;
+        break;
+      case POV_OFF:
+        Serial.println("Turning display off.");
+        showing = false;
+        cycle = false;
+        BlankImage();
+        break;
+      case POV_PICK: 
+        Serial.print("Selecting image ");
+        expect = 1;
+        break;
+      case POV_CYCLE:
+        Serial.println("Cycling through all images.");
+        cycle = true;
+        showing = false;
+        break;
+      default:
+        break;
+    }
+    lastmsg = msg;
+  }
+}
 
 void setup() {
   Serial.begin(9600);
@@ -33,30 +107,44 @@ void setup() {
   delay(200);
   FastLED.setTemperature(TypicalSMD5050); // http://fastled.io/docs/3.1/group___color_enums.html
   FastLED.setBrightness(CurrentBrightness);
+  lastmsg = POV_NO_MSG;
 }
-    
-void PresentImage(unsigned long time, const unsigned int array[]){
-  unsigned long currentTime = millis();
-  while (millis()< currentTime + (time)) {
+
+void BlankImage(){
+     for(int z=NUM_LEDS;z>0;z--){
+     leds[z-1]=0;}
+     FastLED.show();
+}
+
+void PresentImage(const unsigned int array[]){
   int f= numberOfSlices;
   int z; //a counter
   int j=NUM_LEDS;
-  Serial.print("Time before line: ");
-     Serial.println(micros());
     for (int x=0;x<f;x++){
      for(z=NUM_LEDS;z>0;z--){
      leds[z-1]=array[x+((j-z)*f)];}
      FastLED.show();
      delayMicroseconds(40); //increase / testing indicates that this adds a constant to a 136microsecond line drawing time
-     Serial.print("Time after line: ");
-     Serial.println(micros());
-     }    
+    }
     delayMicroseconds(1000); //increase / Testing indicates this adds a constant to a 2 microsecond delay between presentations
-   }
- }
+}
 
-void loop() {  
-   PresentImage(4000,cross); //show image for 4 secs
-   PresentImage(4000,zero);
-   PresentImage(4000,mario);
+
+void loop() {
+  serial_handler();
+  if (showing && (millis() > time_to_stop)) {
+    Serial.println("Turning off picture.");
+    showing = false;
+    BlankImage();
   }
+   if (cycle && (millis() > time_to_stop)) {
+    if (CurrentImage > 1)
+      CurrentImage = 0;
+    else
+      CurrentImage++;
+      time_to_stop = millis() +display_duration;
+  }
+  if (showing || cycle) {
+    PresentImage(imagelib[CurrentImage]); //show image once
+  }
+}
