b77d2206f65c2b50797fca6600b57bff99591157
Rolf Nelson <54694694+SuleymanTheMagnificant@users.noreply.github.com>
2025-10-05 13:41:27 -0400

    Add files via upload

--- Spov_mirror_teensy.ino
+++ Spov_mirror_teensy.ino
@@ -10,23 +10,23 @@ int pos = 0;    // variable to store the servo position
 
 void setup() {
   myservo.attach(9); //servo attached to pin 
-  pinMode(buttonPin, INPUT);   
+  pinMode(buttonPin, INPUT_PULLUP);   
 }
 
 void loop() {
  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) { 
-  for (pos = 10; pos >= 0; pos -= 1) { 
+  if (buttonState == LOW) { 
+  for (pos = 15; pos >= 0; pos -= 1) { 
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(4);                       // waits for the servo to reach the position
   }
-  delay(1000);
+  delay(100);
   for (pos = 0; pos <= 10; pos += 1) { // goes from 10 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(4);                       // waits for the servo to reach the position
   }
-  delay(1000);
+  delay(100);
   }
 
 
