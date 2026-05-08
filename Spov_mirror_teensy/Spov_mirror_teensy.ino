6cda6e4e8cdf49a23fabaf0094d880b8aded11ff
Rolf Nelson <54694694+SuleymanTheMagnificant@users.noreply.github.com>
2026-05-08 14:46:18 -0400

    Add files via upload
    Added file that takes an "x" serial command to rotate servo cw 10deg and then ccw 10deg

--- Spov_mirror_teensy/Spov_mirror_teensy.ino
+++ Spov_mirror_teensy/Spov_mirror_teensy.ino
@@ -1,33 +1,37 @@
-
+//Rotates servo motor 10 degrees clockwise 10deg and then counterclockwise 10deg after receiving a serial "x"
 
 #include <PWMServo.h> //servo library 
 
 PWMServo myservo; 
-const int buttonPin = 2;  
-int buttonState = 0;  
+
+int serialState = 0;  
 
 int pos = 0;    // variable to store the servo position
 
 void setup() {
   myservo.attach(9); //servo attached to pin 
-  pinMode(buttonPin, INPUT_PULLUP);   
+  Serial.begin(9600);
 }
 
 void loop() {
- buttonState = digitalRead(buttonPin);
-  if (buttonState == LOW) { 
-  for (pos = 15; pos >= 0; pos -= 1) { 
+  if(Serial.available()>0){
+    serialState = Serial.read();
+    Serial.print(serialState);
+      if (serialState=='x') { 
+      for (pos = 10; pos >= 0; pos -= 1) { 
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(4);                       // waits for the servo to reach the position
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(4);                       // waits for the servo to reach the position
+  }
+      delay(100);
+        for (pos = 0; pos <= 10; pos += 1) { // goes from 10 degrees to 0 degrees
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(4);                       // waits for the servo to reach the position
   }
   delay(100);
-  for (pos = 0; pos <= 10; pos += 1) { // goes from 10 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(4);                       // waits for the servo to reach the position
+//  serialState=0;
   }
-  delay(100);
-  }
+}
 
 
 }
